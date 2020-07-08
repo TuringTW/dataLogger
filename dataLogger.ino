7a9c9fb8a76c5c622e1d3c25ab707439817ac5d2
turingtw <kevin831130810620@gmail.com>
2020-07-08 22:14:18 +0800

    final version of datalogger

--- dataLogger.ino
+++ dataLogger.ino
@@ -4,16 +4,21 @@
 #include "RTClib.h"
 #include "SdFat.h"
 #include <OneButton.h>
-#include "Adafruit_GFX.h"
-#include "Adafruit_ILI9341.h"
 #include "String.h"
+#include "TFT_ILI9341.h"
+
+#define TFT_GREY 0x7BEF
+
 
 #define CLKSQW 2
-#define BUZZER 4
+#define BUZZER 12
 #define CST0 5
 #define CST1 6
+#define CSSD 3
 #define MAXCLK 8
 #define MAXDO 9
+#define BTN 46
+#define BTNTIME 4
 
 //
 const int numOfReading = 10;
@@ -28,17 +33,15 @@ SdFat sd;
 SdFile file;
 
 // thermocouple sensor
-Adafruit_MAX31855 ts0(5);
-Adafruit_MAX31855 ts1(6);
-// Adafruit_MAX31855 ts0(SCK, 5, MISO);
-// Adafruit_MAX31855 ts1(SCK, 6, MISO);
-// Adafruit_MAX31855 ts0(MAXCLK, 5, MAXDO);
-// Adafruit_MAX31855 ts1(MAXCLK, 6, MAXDO);
+Adafruit_MAX31855 ts0(CST0);
+Adafruit_MAX31855 ts1(CST1);
+
 // TFT LCD
-Adafruit_ILI9341 tft = Adafruit_ILI9341(-1, 13, 12, 11, 10, -1);
+TFT_ILI9341 tft = TFT_ILI9341(); 
 
 // button
-OneButton btnLog(A5, LOW, true);
+OneButton btnLog(BTN, true);
+OneButton btnTime(BTNTIME, true);
 
 
 bool _isPPS = false;
@@ -52,175 +55,119 @@ int _curRefreshTime = 0;
 
 int b;
 int idx = 0; //smoothing idx
-int storePeriod[4] = {1, 10, 60, 3600};
+int storePeriod[5] = {1, 10, 60, 600,3600};
 int _pIdx=1;
+short _prevIdx = 1; //for file system
+
+// external interrupt
+void onePPS(){ 
+  noInterrupts();
+  _isPPS = true;
+  TCNT1 = 12449; //set timer1 counter 1 step before interrupt (pulse out), so that there'be a pulse right after receiving PPS signal
+  interrupts();
+}
+//5Hz timer interrupt
+ISR(TIMER1_COMPA_vect){ //5Hz
+	_isNotRefresh = true;
+}
+
+
 
 void setup() {
+	Serial.begin(115200);
+
 	pinMode(A0, INPUT);
 	pinMode(A1, INPUT);
 	pinMode(A2, INPUT);
 	pinMode(A3, INPUT);
-	pinMode(A4, INPUT);
 	pinMode(CLKSQW, INPUT);
 	pinMode(BUZZER, OUTPUT);
+	pinMode(CSSD, OUTPUT);
+	pinMode(CST0, OUTPUT);
+	pinMode(CST1, OUTPUT);
 //  init serial monitor
-	Serial.begin(115200);
+	
+ 
 // initialize LCD  
-	tft.begin();
-	tft.fillScreen(ILI9341_BLACK);
+	digitalWrite(CST0, HIGH);
+	digitalWrite(CST1, HIGH);
+	digitalWrite(CSSD, HIGH);
+	tft.init();
+	tft.setRotation(2);
+	tft.fillScreen(TFT_BLACK);
+	// Serial.println("done8");
+  
 // init clock module
 	initClock();
+	// Serial.println("done7");
+
 // init SD module
-	initSD(); 
+  initSDFile(); 
+	// Serial.println("done6");
+
 // initialize interrupt
-	attachInterrupt(digitalPinToInterrupt(CLKSQW), pps, RISING);
-	Timer1.initialize(100000);
-	Timer1.attachInterrupt(readData);
+	TimerInit();	
+	attachInterrupt(digitalPinToInterrupt(2), onePPS, RISING);
+	// Serial.println("done5");
+
 // set external voltage reference
 	analogReference(INTERNAL2V56);
+	// Serial.println("done4");
+
 // button
 	btnLog.attachClick(singleClickLog);
+  btnTime.attachClick(singleClickTime);
+	// Serial.println("done3");
+
+
+	// Serial.println("done2");
+
+// inittialize thermocoutple
+	ts0.begin();
+	ts1.begin();
+	// Serial.println("done");
 
 // initialize smoothing param
 	idx = 0;
 	_isLogging = false;
 	_isNotRefresh = true;
-// inittialize thermocoutple
-	ts0.begin();
-	ts1.begin();
-}
-void pps(){
-	_isPPS = true;
+// initialize LCD
+  printTime();
+  printSDStatus();
+  printInterval();
+  printLogStatus();
+  printVoltage();
+  printTemperature();
 }
 
 void loop() {
-		
-	if(_isLogging){
-		if(_isPPS){
-			_isPPS = false;
-			_curtime++;
+  
+	if (_isPPS){
+    printSerialVoltage();
+    Serial.println();
+		printTime();
+		printVoltage();
+		printTemperature();	
+
+		if(_isLogging){
+			
 			_curtime = _curtime%storePeriod[_pIdx];
 			if (_curtime==0)
 			{
 				storeData();
 			}
+      _curtime++;
 		}
+		_isPPS = false;
 	}
 	if(_isNotRefresh){
 		_curRefreshTime++;
-		if (_curRefreshTime>=2)
-		{
-			printVoltage();
-			printTemperature();
-			printTime();
-			_curRefreshTime = 0;
-		}
+		readData();
 		_isNotRefresh = false;
 			
 	}
-
+  if(!_isLogging){
+    btnTime.tick();  
+  }
 	btnLog.tick();
-}
-void printVoltage(){
-	char temp[7];
-	
-	for(int i=0;i<4;i++){
-		tft.setCursor(0, 0+60*i);
-  		tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);  tft.setTextSize(2);
-  		tft.print("V ");
-  		tft.print(i+1);
-  		tft.setCursor(0, 20+60*i);
-  		tft.setTextColor(ILI9341_RED, ILI9341_BLACK);  tft.setTextSize(3);
-  		dtostrf(data[i][numOfReading], 2, 1, temp);
-  		tft.print(padspace(String(temp), 4));
-  		tft.setTextSize(1);
-  		tft.print(" V");
-  		
-		
-	}
-}
-void printTemperature(){
-	char temp[7];
-	
-	for(int i=4;i<8;i++){
-		tft.setCursor(120, 0+60*(i-4));
-  		tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);  tft.setTextSize(2);
-  		tft.print("T ");
-  		tft.print(i-3);
-  		tft.setCursor(120, 20+60*(i-4));
-  		tft.setTextColor(ILI9341_BLUE, ILI9341_BLACK);  tft.setTextSize(3);
-  		dtostrf(data[i][numOfReading], 2, 1, temp);
-  		tft.print(padspace(String(temp), 5));
-  		tft.setTextSize(1);
-  		tft.print(" C");
-	}	
-}
-void printTime(){
-	DateTime now = rtc.now();
-	char buffer[50]={0};
-	sprintf(buffer, "%04d/%02d/%02d", now.year(), now.month(), now.day());
-	tft.setCursor(0, 250);
-  	tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);  tft.setTextSize(2);
-  	tft.print(buffer);
-  	sprintf(buffer, "%02d:%02d", now.hour(), now.minute());
-  	tft.setCursor(0, 270);
-  	tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);  tft.setTextSize(4);
-  	tft.print(buffer);
-}
-void storeData(){
-		char buffer[100]={0};
-		char temp[7];
-		DateTime now = rtc.now();
-		sprintf(buffer, "%04d%02d%02dT%02d%02d%02d+0800,", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
-
-		
-		
-		noInterrupts();
-		for(int i=0;i<numOfData;i++){
-			dtostrf(data[i][numOfReading], 2, 2, temp); 
-			sprintf(buffer+strlen(buffer), "%s,", temp);    
-		}
-		interrupts();
-		
-		sprintf(buffer+strlen(buffer), "\n");  
-		Serial.print(buffer);
-		
-		file.print(buffer);
-		file.flush();  
-		double tuneValue = analogRead(A4);
-		int interval = map(tuneValue, 0, 1024, 1, 6)*1000;
-	
-		
-
-		tone(BUZZER, 300);
-		delay(50);
-		noTone(BUZZER);
-	
-}
-void readData(){
-	data[0][idx] = (analogRead(A0)+0.5)/1024.0*26.3977762880763;
-	data[1][idx] = (analogRead(A1)+0.5)/1024.0*26.3977762880763;
-	data[2][idx] = (analogRead(A2)+0.5)/1024.0*26.3977762880763;
-	data[3][idx] = (analogRead(A3)+0.5)/1024.0*26.3977762880763;
-	data[4][idx] = ts0.readCelsius();
-	data[5][idx] = ts1.readCelsius();
-	data[6][idx] = ts0.readInternal();
-	data[7][idx] = ts1.readInternal();
-	idx+=1;
-	if(idx>=numOfReading)idx=0;
-// average
-	for(int i=0;i<numOfData;i++){
-		data[i][numOfReading]=0;
-		for(int j=0; j<numOfReading;j++){
-			data[i][numOfReading]+=data[i][j];
-		}
-		data[i][numOfReading]/=numOfReading;
-	}
-	_isNotRefresh = true;
-}
-String padspace(String x, int len){
-	while(x.length()<len){
-		x = " "+x;
-	}
-	return x;
 }
--- util.ino
+++ util.ino
@@ -1,4 +1,25 @@
 //  initialize clock
+void TimerInit()
+{
+// timer1 for pulser
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0; //reset timer1 counter 
+
+  TCCR1B |= (1 << WGM12); // CTC Mode(Clear timer on compare match)
+  TCCR1B |= (1 << CS12);  // 256 prescalar: setCS11 = 1
+
+// for pulse output
+  OCR1A = 12500; // compare register of counterA
+  TIMSK1 |= (1 << OCIE1A); // Enable timer compare interrupt
+
+
+  
+  interrupts();
+}
+
+
 void initClock(){
 	if (! rtc.begin()) {
 		tft.setCursor(0, 0); // 設定游標位置在第一行行首
@@ -8,38 +29,276 @@ void initClock(){
 	Ds3231SqwPinMode mode = Ds3231SqwPinMode::DS3231_SquareWave1Hz; //output 1Hz at SQW pin
 	rtc.writeSqwPinMode(mode);
 	if (rtc.lostPower()) {
-		tft.setCursor(0, 0); // 設定游標位置在第一行行首
+		// tft.setCursor(0, 0); // 設定游標位置在第一行行首
 		// tft.print(F("Clock lost power"));
-		tft.setCursor(0, 1); // 設定游標位置在第一行行首
+		// tft.setCursor(0, 1); // 設定游標位置在第一行行首
 		// tft.print(F("Pls set the time!"));
-		while(1){tone(BUZZER, 300, 200);noTone(BUZZER);delay(200);}
+		// while(1){tone(BUZZER, 300, 200);noTone(BUZZER);delay(200);}
 	}
 }
-void initSD(){
+void initSDFile(){
 // initialize SD card  
-	if(sd.begin(SS, SD_SCK_MHZ(50))){
-		DateTime now = rtc.now();
-		sprintf(fileName, "L%d%02d%02d_%02d%02d%02d", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
+	if(sd.begin(CSSD, SD_SCK_MHZ(50))){
+    _isSD = true;
 		
-		if(!file.open(fileName, O_CREAT | O_WRITE))_isSD = false;
-		else _isSD = true;
+    short i = 0;
+    do{
+      i++;
+      sprintf(fileName, "L%03d", i);
+    }while(sd.exists(fileName));
+    _prevIdx = i;
+    sprintf(fileName, " OK ");
 	}
-	if(!_isSD){
-		tft.setCursor(0, 0); // 設定游標位置在第一行行首
-		tft.print(F("SD error"));
-		while (1){tone(BUZZER, 300, 200);noTone(BUZZER);delay(200);}
-	}
+}
+void getLogFile(){
+	short i = _prevIdx;
+	do{
+		i++;
+		sprintf(fileName, "L%03d", i);
+		Serial.println(fileName);
+	}while(sd.exists(fileName));
+  _prevIdx = i;
+	Serial.println("hello");
+	if(!file.open(fileName, O_CREAT | O_WRITE))_isSD = false;
+	else _isSD = true;
 }
 void singleClickLog(){
 	Serial.println(F("logging"));
-	if (_isLogging){_isLogging = false;}
+	if (_isLogging){
+		_isLogging = false;
+		file.close();
+    printLogStatus();
+
+    tone(BUZZER, 500);
+    delay(100);
+    noTone(BUZZER);
+    delay(100);
+    tone(BUZZER, 400);
+    delay(100);
+    noTone(BUZZER);
+    delay(100);
+    tone(BUZZER, 300);
+    delay(100);
+    noTone(BUZZER);
+    delay(100);
+	}
 	else{
+		getLogFile();
+    printSDStatus();
 		if (_isSD)
 		{
 			_curtime = 0;
 			_isLogging = true;
+      printLogStatus();
+      for(int i=3;i<=5;i++){
+          tone(BUZZER, i*100);
+          delay(100);
+          noTone(BUZZER);
+          delay(100);
+      }
 		}else{
-			// show no sd card
+      if(sd.begin(CSSD, SD_SCK_MHZ(50))){
+        _isSD = true;
+        sprintf(fileName, " OK ");
+        printSDStatus();
+        short i = 0;
+        do{
+          i++;
+          sprintf(fileName, "L%03d", i);
+          Serial.println(fileName);
+        }while(sd.exists(fileName));
+        _prevIdx = i-1;
+        singleClickLog();
+      }else{
+  			_isLogging = false;
+        for(int i=0;i<5;i++){
+          tone(BUZZER, 300);
+          delay(100);
+          noTone(BUZZER);
+          delay(50);
+          tone(BUZZER, 300);
+          delay(100);
+          noTone(BUZZER);
+          delay(200);
+        }
+      }
 		}
+      
+    
+    
+    
 	}
+}
+void singleClickTime(){
+  _pIdx=(_pIdx+1)%5;
+  tone(BUZZER, 600);
+  printInterval();
+  delay(50);
+  tone(BUZZER, 700);
+  delay(50);
+  noTone(BUZZER);
+  
+}
+void printVoltage(){
+  char temp[7];
+  int startPos = 92;
+  for(int i=0;i<4;i++){
+    tft.setCursor(0, startPos+60*i,1);
+      tft.setTextColor(TFT_WHITE, TFT_BLACK);  tft.setTextSize(2);
+      tft.print("V ");
+      tft.print(i+1);
+      tft.setCursor(0, startPos+20+60*i);
+      tft.setTextColor(TFT_RED, TFT_BLACK);  tft.setTextSize(3);
+      dtostrf(data[i][numOfReading], 2, 1, temp);
+      tft.print(padspace(String(temp), 4));
+      tft.setTextSize(1);
+      tft.print(" V");
+      
+    
+  }
+}
+void printSerialVoltage(){
+  char temp[10];
+  for(int i=0;i<4;i++){
+      dtostrf(data[i][numOfReading], 2, 4, temp);
+      Serial.print(temp);
+      Serial.print("\t"); 
+  }
+}
+void printTemperature(){
+  char temp[7];
+  int startPos = 92;
+  for(int i=4;i<8;i++){
+    tft.setCursor(120, startPos+60*(i-4),1);
+      tft.setTextColor(TFT_WHITE, TFT_BLACK);  tft.setTextSize(2);
+      tft.print("T ");
+      tft.print(i-3);
+      tft.setCursor(120, startPos+20+60*(i-4));
+      tft.setTextColor(TFT_BLUE, TFT_BLACK);  tft.setTextSize(3);
+      dtostrf(data[i][numOfReading], 2, 1, temp);
+      tft.print(padspace(String(temp), 5));
+      tft.setTextSize(1);
+      tft.print(" C");
+  } 
+}
+void printTime(){
+  DateTime now = rtc.now();
+  char buffer[50]={0};
+  sprintf(buffer, " %04d/%02d/%02d", now.year(), now.month(), now.day());
+  tft.setCursor(0, 0, 1);
+    tft.setTextSize(2);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.print(buffer);
+    if (now.second()%2==0){
+      sprintf(buffer, "%02d:%02d", now.hour(), now.minute()); 
+    }else{
+      sprintf(buffer, "%02d %02d", now.hour(), now.minute()); 
+    }
+    tft.setTextSize(1);
+    tft.setCursor(0, 28, 7);
+    tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+    tft.print(buffer);
+}
+void printSDStatus(){
+  tft.setCursor(150, 0, 1);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);  tft.setTextSize(2);
+  tft.print("SD:");
+  if(_isSD){
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);  tft.setTextSize(2);
+    tft.print(fileName);
+  }else{
+    tft.setTextColor(TFT_RED, TFT_BLACK);  tft.setTextSize(2);
+    tft.print(" NO ");
+  }
+}
+void printInterval(){
+  tft.setCursor(150, 25, 1);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);  tft.setTextSize(2);
+  tft.print("TI:");
+  char temp[7];
+  int output=storePeriod[_pIdx];
+  char unit='s';
+  if(output/60>0){
+    output /= 60;  
+    unit='m';
+    if(output/60>0){
+      output/=60;
+      unit='h';  
+    }
+  }
+  sprintf(temp, "%3d", output);
+  tft.print(temp);
+  tft.print(unit);
+}
+void printLogStatus(){
+  if(_isLogging){
+    tft.fillRoundRect(150,45,85,30, 5, TFT_RED);  
+    tft.setCursor(167,50);
+    tft.setTextSize(3);
+    tft.setTextColor(TFT_BLACK, TFT_RED);
+    tft.print("REC"); 
+  }else{
+    tft.fillRoundRect(150,45,85,30, 5, TFT_GREEN);  
+    tft.setCursor(159,50);
+    tft.setTextSize(3);
+    tft.setTextColor(TFT_BLACK,TFT_GREEN);
+    tft.print("STBY");
+  }
+  
+}
+void storeData(){
+    char buffer[100]={0};
+    char temp[7];
+    DateTime now = rtc.now();
+    sprintf(buffer, "%04d%02d%02dT%02d%02d%02d+0800,", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
+
+    
+    
+    noInterrupts();
+    for(int i=0;i<numOfData;i++){
+      dtostrf(data[i][numOfReading], 2, 2, temp); 
+      sprintf(buffer+strlen(buffer), "%s,", temp);    
+    }
+    interrupts();
+    
+    sprintf(buffer+strlen(buffer), "\n");  
+    Serial.print(buffer);
+    
+    file.print(buffer);
+    file.flush();  
+    double tuneValue = analogRead(A4);
+    int interval = map(tuneValue, 0, 1024, 1, 6)*1000;
+  
+    
+
+    tone(BUZZER, 500);
+    delay(100);
+    noTone(BUZZER);
+  
+}
+void readData(){
+  data[0][idx] = (analogRead(A0)+0.5)/1024.0*46.7876549+0.13679976;
+  data[1][idx] = (analogRead(A1)+0.5)/1024.0*46.7876549+0.13679976;
+  data[2][idx] = (analogRead(A2)+0.5)/1024.0*46.7876549+0.13679976;
+  data[3][idx] = (analogRead(A3)+0.5)/1024.0*46.7876549+0.13679976;
+  data[4][idx] = ts0.readCelsius();
+  data[5][idx] = ts1.readCelsius();
+  data[6][idx] = ts0.readInternal();
+  data[7][idx] = ts1.readInternal();
+  idx+=1;
+  if(idx>=numOfReading)idx=0;
+// average
+  for(int i=0;i<numOfData;i++){
+    data[i][numOfReading]=0;
+    for(int j=0; j<numOfReading;j++){
+      data[i][numOfReading]+=data[i][j];
+    }
+    data[i][numOfReading]/=numOfReading;
+  }
+}
+String padspace(String x, int len){
+  while(x.length()<len){
+    x = " "+x;
+  }
+  return x;
 }
